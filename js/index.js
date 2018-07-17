cd8459787571260464f9ce2515dfa2f4225f071e
Евгения Масликова < >
2018-07-18 00:30:33 +0300

    fixes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,86 @@
-var current_fs,next_fs,previous_fs;var left,opacity,scale;var animating;var count=1;var current_fs=$('.question');var base_question=$('.double_section');$("#previous").click(function(){var previous_fs=$(current_fs).prev();$(current_fs).removeClass('question');previous_fs.addClass('question');current_fs.hide('slow');previous_fs.fadeIn(800);current_fs=previous_fs;count--;if(count<5){$('#send').hide('slow');$('.next').fadeIn(800);}if(count==1){$('#previous').hide('slow');}if(count<2){$('#previous').hide('slow');}});$('.next').click(function(){var next_fs=current_fs.next();$(current_fs).removeClass('question');next_fs.addClass('question');current_fs.hide('slow');next_fs.fadeIn(800);count++;if(count>1){$('#previous').css('display','inline-block');$('#previous').fadeIn(800);}if(count>4){$('.next').hide('slow');$('#send').fadeIn(800);}current_fs=next_fs;});$(document).ready(function(){$('.get_offer').click(function(){var scroll_el=$(this).attr('href');if($(scroll_el).length!=0){$('html, body').animate({scrollTop:$(scroll_el).offset().top},2000);}return false;});});$(document).ready(function(){function a(){$("body").scrollspy({target:".scroll-nav",offset:100})}$(function(){$(window).scroll(a),a()})}),$(document).ready(function(){$("body").on("click",'[href*="#"]',function(a){var b=100;$("html,body").stop().animate({scrollTop:$(this.hash).offset().top-b},1e3),a.preventDefault()})})
+var current_fs, next_fs, previous_fs;
+var left, opacity, scale;
+var animating;
+var count = 1;
+var current_fs = $('.question');
+var base_question = $('.double_section');
+
+$("#previous").click(function() {
+    var previous_fs = $(current_fs).prev();
+    $(current_fs).removeClass('question');
+    previous_fs.addClass('question');
+    current_fs.hide('slow');
+    previous_fs.fadeIn(800);
+    current_fs = previous_fs;
+    count--;
+    if (count == 1) {
+        $('#previous').hide('slow');
+    }
+    if (count < 2) {
+        $('#previous').hide('slow');
+    }         
+    
+});
+
+
+$('#close_test_form').click(function() {
+$('#blindLayer').css('display', 'none');    
+$(current_fs).hide('fast');
+$(base_question).show('fast');
+$('.count').css('opacity', 1);
+current_fs= base_question;
+count = 1;
+
+}
+);
+
+
+
+$('.checked_answer').click(function() {
+    var next_fs = current_fs.next();
+    $(current_fs).removeClass('question');
+    next_fs.addClass('question');
+    current_fs.hide('slow');
+    next_fs.fadeIn(800);
+    count++;
+    if (count > 1) {
+        $('#previous').css('display', 'inline-block');
+        $('#previous').fadeIn(800);
+        $('.count').css('opacity', 0);
+    }
+    if (count > 4) {       
+        $('#previous').hide('fast');
+        $('#blindLayer').css('display', 'block');      
+    }
+ 
+    current_fs = next_fs;
+});
+$(document).ready(function() {
+    $('.get_offer').click(function() {
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 2000);
+        }
+        return false;
+    });
+});
+$(document).ready(function() {
+    function a() {
+        $("body").scrollspy({
+            target: ".scroll-nav",
+            offset: 100
+        })
+    }
+    $(function() {
+        $(window).scroll(a), a()
+    })
+}), $(document).ready(function() {
+    $("body").on("click", '[href*="#"]', function(a) {
+        var b = 100;
+        $("html,body").stop().animate({
+            scrollTop: $(this.hash).offset().top - b
+        }, 1e3), a.preventDefault()
+    })
+})
